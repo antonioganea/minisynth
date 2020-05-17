20426c65a0dc07f2526f076af7a060b4ecaf2daa
Antonio Alexandru Ganea <antonio.ganea@yahoo.com>
2020-05-17 12:58:58 +0300

    More splitting

--- include/VirtualController.hpp
+++ include/VirtualController.hpp
@@ -7,76 +7,14 @@
 
 #include <iostream>
 
-const char bayanOrder[] = "zswxdecfrvgtbhynjumki,lo.;p/'[";
-
-const int inputOrder[30] = {
-    sf::Keyboard::Z,
-    sf::Keyboard::S,
-    sf::Keyboard::W,
-
-    sf::Keyboard::X,
-    sf::Keyboard::D,
-    sf::Keyboard::E,
-
-    sf::Keyboard::C,
-    sf::Keyboard::F,
-    sf::Keyboard::R,
-
-    sf::Keyboard::V,
-    sf::Keyboard::G,
-    sf::Keyboard::T,
-
-    sf::Keyboard::B,
-    sf::Keyboard::H,
-    sf::Keyboard::Y,
-
-    sf::Keyboard::N,
-    sf::Keyboard::J,
-    sf::Keyboard::U,
-
-    sf::Keyboard::M,
-    sf::Keyboard::K,
-    sf::Keyboard::I,
-
-    sf::Keyboard::Comma,
-    sf::Keyboard::L,
-    sf::Keyboard::O,
-
-    sf::Keyboard::Period,
-    sf::Keyboard::SemiColon,
-    sf::Keyboard::P,
-
-    sf::Keyboard::Slash,
-    sf::Keyboard::Quote,
-    sf::Keyboard::LBracket,
-};
-
 
 class VirtualController {
-    int getOrder(char ch){
-        char * location = strchr(bayanOrder, tolower(ch));
-        if ( location == NULL ) return -1;
-        return location-bayanOrder;
-    }
-    int inputs = sizeof(inputOrder) / sizeof(int);
-    VirtualController() {
-
-    }
-
+    int getOrder(char ch);
+    VirtualController();
     static constexpr VirtualController* instance = 0;
-
     public:
-    int getInputOrder(int key) {
-        for ( int i = 0; i < 30; i++ ) {
-            if ( inputOrder[i] == key ){
-                return i;
-            }
-        }
-        return -1;
-    }
-    static VirtualController* getInstance() {
-        return instance;
-    }
+    int getInputOrder(int key);
+    static VirtualController* getInstance();
 };
 
 #endif // VIRTUALCONTROLLER_HPP_INCLUDED
--- src/VirtualController.cpp
+++ src/VirtualController.cpp
@@ -1,1 +1,73 @@
 #include <VirtualController.hpp>
+
+#include <SFML/Window/Keyboard.hpp>
+#include <cstring>
+#include <ctype.h>
+
+const char bayanOrder[] = "zswxdecfrvgtbhynjumki,lo.;p/'[";
+
+const int inputOrder[30] = {
+    sf::Keyboard::Z,
+    sf::Keyboard::S,
+    sf::Keyboard::W,
+
+    sf::Keyboard::X,
+    sf::Keyboard::D,
+    sf::Keyboard::E,
+
+    sf::Keyboard::C,
+    sf::Keyboard::F,
+    sf::Keyboard::R,
+
+    sf::Keyboard::V,
+    sf::Keyboard::G,
+    sf::Keyboard::T,
+
+    sf::Keyboard::B,
+    sf::Keyboard::H,
+    sf::Keyboard::Y,
+
+    sf::Keyboard::N,
+    sf::Keyboard::J,
+    sf::Keyboard::U,
+
+    sf::Keyboard::M,
+    sf::Keyboard::K,
+    sf::Keyboard::I,
+
+    sf::Keyboard::Comma,
+    sf::Keyboard::L,
+    sf::Keyboard::O,
+
+    sf::Keyboard::Period,
+    sf::Keyboard::SemiColon,
+    sf::Keyboard::P,
+
+    sf::Keyboard::Slash,
+    sf::Keyboard::Quote,
+    sf::Keyboard::LBracket,
+};
+
+constexpr int inputs = sizeof(inputOrder) / sizeof(int);
+
+int VirtualController::getOrder(char ch){
+    char * location = strchr(bayanOrder, tolower(ch));
+    if ( location == NULL ) return -1;
+    return location-bayanOrder;
+}
+
+VirtualController::VirtualController() {
+
+}
+static constexpr VirtualController* instance = 0;
+int VirtualController::getInputOrder(int key) {
+    for ( int i = 0; i < 30; i++ ) {
+        if ( inputOrder[i] == key ){
+            return i;
+        }
+    }
+    return -1;
+}
+VirtualController* VirtualController::getInstance() {
+    return instance;
+}
